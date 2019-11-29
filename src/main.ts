313afdcf201f89d3931ec024c8d8df60927690b0
Alexander Kireev <farvistadw@gmail.com>
2019-11-30 02:20:58 +0700

    added postgres

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
+import { Logger, INestApplication } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+    Logger.log(`server started on port ${port}`, 'Bootstrap');
 }
 bootstrap();
