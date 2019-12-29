ee3cf7efaf87a0be3bc36cfc48b8e675c12a6407
Alexander Kireev <farvistadw@gmail.com>
2019-12-29 14:27:21 +0700

    added chart query

--- src/market-data/market-data.resolver.ts
+++ src/market-data/market-data.resolver.ts
@@ -1,6 +1,7 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
 import { MarketDataService } from './market-data.service';
 import { SymbolDTO } from './symbol.dto';
+import { HistoricalPrice } from 'iex-cloud';
 
 @Resolver('MarketData')
 export class MarketDataResolver {
@@ -9,5 +10,11 @@ export class MarketDataResolver {
     @Query()
     search(@Args('fragment') fragment: string): SymbolDTO[] {
         return this.marketDataService.search(fragment);
+    }
+
+    @Query()
+    async chart(@Args('symbol') symbol: string): Promise<ReadonlyArray<Partial<HistoricalPrice>>> {
+        const result = await this.marketDataService.chart(symbol);
+        return result;
     }
 }
--- src/market-data/market-data.service.ts
+++ src/market-data/market-data.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { historicalPrices, HistoricalPrice } from 'iex-cloud';
+
 import { Symbols } from '../shared/tickers';
 import { SymbolDTO } from './symbol.dto';
 
@@ -7,5 +9,11 @@ export class MarketDataService {
     search(fragment: string): SymbolDTO[] {
         const symbols = Symbols as SymbolDTO[];
         return symbols.filter(x => x.symbol.includes(fragment.toUpperCase())).slice(0, 10);
+    }
+
+    async chart(ticker: string): Promise<ReadonlyArray<Partial<HistoricalPrice>>> {
+        // console.log(ticker);
+
+        return await historicalPrices(ticker, '5d');
     }
 }
